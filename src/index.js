b94bd720fab23f89cce3bbe4eea47fe9cfb86ad8
Vivian Gichure <viviangichure@gmail.com>
2025-06-20 11:12:54 +0300

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent page refresh
+
+    const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => {
+        if (!response.ok) throw new Error("Movie not found");
+        return response.json();
+      })
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+      .catch((error) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = "Movie not found";
+        summary.innerText = "";
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
